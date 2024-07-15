5a1895e5cf0323b12e0442024aafe21e0f7b24fa
taylorallenshc <tayallen@stanfordhealthcare.org>
2024-07-15 03:14:35 -0700

    style: fixed MRN input styling

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -147,7 +147,7 @@
 
 				<input
 					type="text"
-					class="mr-3 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+					class="mr-3 px-3 py-2 border border-gray-600 bg-gray-800 text-gray-200 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
 					placeholder="Enter MRN"
 					bind:value={mrn}
 				/>
